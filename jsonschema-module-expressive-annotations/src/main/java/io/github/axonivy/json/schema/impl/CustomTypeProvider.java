b211d49afc0dbc7fe3270761554c567bd5fdf44d
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-11-17 14:18:06 +0100

    Support nested @CustomType definitions
    - for those who have route many fields, a wrapper type is handy

--- jsonschema-module-expressive-annotations/src/main/java/io/github/axonivy/json/schema/impl/CustomTypeProvider.java
+++ jsonschema-module-expressive-annotations/src/main/java/io/github/axonivy/json/schema/impl/CustomTypeProvider.java
@@ -1,6 +1,11 @@
 package io.github.axonivy.json.schema.impl;
 
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import com.fasterxml.classmate.ResolvedType;
+import com.github.victools.jsonschema.generator.AnnotationHelper;
 import com.github.victools.jsonschema.generator.CustomDefinition;
 import com.github.victools.jsonschema.generator.CustomDefinitionProviderV2;
 import com.github.victools.jsonschema.generator.CustomPropertyDefinition;
@@ -12,25 +17,27 @@ import io.github.axonivy.json.schema.annotations.CustomType;
 
 public class CustomTypeProvider implements CustomPropertyDefinitionProvider<FieldScope>, CustomDefinitionProviderV2 {
 
+  private static final Predicate<Annotation> CONSIDER_NESTED = a -> !a.toString().startsWith("@java.lang.annotation");
+
   @Override
   public CustomPropertyDefinition provideCustomSchemaDefinition(FieldScope scope, SchemaGenerationContext context) {
-    CustomType reference = scope.getAnnotation(CustomType.class);
-    if (reference == null) {
+    var reference = Optional.ofNullable(scope.getAnnotation(CustomType.class, CONSIDER_NESTED));
+    if (reference.isEmpty()) {
       return null;
     }
 
-    ResolvedType resolvedType = context.getTypeContext().resolve(reference.value());
+    ResolvedType resolvedType = context.getTypeContext().resolve(reference.get().value());
     var definition = context.createDefinitionReference(resolvedType);
     return new CustomPropertyDefinition(definition);
   }
 
   @Override
   public CustomDefinition provideCustomSchemaDefinition(ResolvedType javaType, SchemaGenerationContext context) {
-    CustomType reference = javaType.getErasedType().getAnnotation(CustomType.class);
-    if (reference == null) {
+    var reference = AnnotationHelper.resolveAnnotation(javaType.getErasedType(), CustomType.class, CONSIDER_NESTED);
+    if (reference.isEmpty()) {
       return null;
     }
-    ResolvedType resolvedType = context.getTypeContext().resolve(reference.value());
+    ResolvedType resolvedType = context.getTypeContext().resolve(reference.get().value());
     var definition = context.createStandardDefinition(resolvedType, this);
     return new CustomDefinition(definition);
   }
